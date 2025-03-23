293b72bb51dcb34e1146332eb51c5fbb1022a3ce
alin.2018.aap <alin.2018.aap@gmail.com>
2025-03-23 05:58:52 +0000

    add comment

--- PrebuiltReActAgent/tool_calling_concept.py
+++ PrebuiltReActAgent/tool_calling_concept.py
@@ -7,10 +7,10 @@ import os
 # .envファイルのパスを指定して読み込む
 load_dotenv('.env')
 
-# toolの定義
+# 1 Tool Callingの関数を定義する
 @tool
 def add(a: int, b: int) -> int:
-    """2つの値を足し算して返す"""
+    """LLMで計算されることと違うように、2つの値を足し算し、再プラス２で返す"""
     return a + b + 2
 
 # プロンプトの定義
@@ -35,12 +35,12 @@ llm = ChatOpenAI(
     max_retries=2,
     api_key=os.getenv("OPENAI_API_KEY")
 )
-# bind_toolsでLLMモデルとtoolを紐づける
+# 2 定義した関数をLLMの実行に紐づける
 chain = prompt | llm.bind_tools([add])
-# LLMから関数の呼び出し結果を取得
-result = chain.invoke({'messages': ['3 + 4']})
+result = chain.invoke({'messages': ['2000 + 23']})
+# result = chain.invoke({'messages': ['hello world']}) # output: result.tool_calls -> IndexError: list index out of range
 
 # 引数のみで呼び出す
-print(add.invoke(result.tool_calls[0]['args']))
-# LLMからtool_callの値をそのままadd関数に渡して関数を実行
-print(add.invoke(result.tool_calls[0]))
+print(add.invoke(result.tool_calls[0]['args'])) # output: 2025
+# 3 LLMから呼び出された関数を実際に実行する
+print(add.invoke(result.tool_calls[0])) # output: content='2025' name='add' tool_call_id='call_gvxfJGwhPPa7mOtl8Rt6Fegv'
